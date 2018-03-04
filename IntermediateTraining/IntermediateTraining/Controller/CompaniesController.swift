4c6afa2b8972f88fe9f38ae19ea5f2a839ac6f48
Kylta <Christophe.bugnon02@gmail.com>
2018-03-04 16:56:28 +0100

    3. Core Data Entities : Deleting Companies

--- IntermediateTraining/IntermediateTraining/Controller/CompaniesController.swift
+++ IntermediateTraining/IntermediateTraining/Controller/CompaniesController.swift
@@ -72,6 +72,37 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelegat
         present(navBarController, animated: true, completion: nil)
     }
     
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { (_, indexPath) in
+            let company = self.companies[indexPath.row]
+            print("Attempting to delete company", company.name ?? "")
+            
+            // Remove our company from our tableView
+            self.companies.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            // delete the company from the Core Data
+            let context = CoreDataManager.shared.persistentContrainer.viewContext
+            
+            context.delete(company)
+            
+            do {
+                try context.save()
+            } catch let saveErr {
+                print("Failed to delete company", saveErr)
+            }
+            
+        }
+        
+        let editAction = UITableViewRowAction(style: .normal, title: "Edit") { (_, indexPath) in
+            print("Attemps to edit company")
+        }
+        
+        return [deleteAction, editAction]
+        
+    }
+    
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let view = UIView()
         view.backgroundColor = .lightBlue
