db9b4d74bd578a66c2f6f9f372dd66d4ffdeb856
Kylta <Christophe.bugnon02@gmail.com>
2018-03-05 11:54:15 +0100

    4. Core Data Attributes Types : Photo Selector

--- IntermediateTraining/IntermediateTraining/Controller/CreateCompanyController.swift
+++ IntermediateTraining/IntermediateTraining/Controller/CreateCompanyController.swift
@@ -15,7 +15,7 @@ protocol CreateCompanyControllerDelegate {
     func didEditCompany(company: Company)
 }
 
-class CreateCompanyController: UIViewController {
+class CreateCompanyController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var company: Company? {
         didSet {
@@ -34,6 +34,43 @@ class CreateCompanyController: UIViewController {
         uv.translatesAutoresizingMaskIntoConstraints = false
         return uv
     }()
+    
+    lazy var companyImageView: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "select_photo_empty"))
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectPhoto)))
+        return iv
+    }()
+    
+    @objc private func handleSelectPhoto() {
+        print("Trying to select photo...")
+        
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            
+            companyImageView.image = editedImage
+            
+        } else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            companyImageView.image = originalImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     let nameLabel: UILabel = {
         let label = UILabel()
@@ -130,10 +167,16 @@ class CreateCompanyController: UIViewController {
         lightBackgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         lightBackgroundView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         lightBackgroundView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        lightBackgroundView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        lightBackgroundView.heightAnchor.constraint(equalToConstant: 350).isActive = true
+        
+        view.addSubview(companyImageView)
+        companyImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8).isActive = true
+        companyImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        companyImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        companyImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
     
         view.addSubview(nameLabel)
-        nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: companyImageView.bottomAnchor).isActive = true
         nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
         nameLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
         nameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
